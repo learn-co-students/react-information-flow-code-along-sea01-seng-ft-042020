01c1fa4d212f21953c22eec816935fecf2fd946a
Joshua Mclean <joshmcleana@gmail.com>
2020-06-23 19:10:58 -0700

    Done.

--- src/Parent.js
+++ src/Parent.js
@@ -7,15 +7,26 @@ class Parent extends Component {
   constructor() {
     super()
     this.state = {
-      color: getRandomColor()
+      color: getRandomColor(),
+      childrenColor: '#FFF' // D2. The parent can control the color of it's children and apply similar random color functionality
     }
+  }
+  // D1-3 making color changing arrow function useing setState to update the state
+  // D2 giving the changeColor method an argument (sending data BACK to the parent from child) 
+  changeColor = (newChildColor) => {
+    this.setState({
+      color: getRandomColor(),
+      childrenColor: newChildColor
+    })
   }
 
   render() {
     return (
       <div className="parent" style={{backgroundColor: this.state.color}}>
-        <Child />
-        <Child />
+        {/* D1. passing the changColor function as a prop to the children elements */}
+        {/* D2. Passing the data that represents the color of the children down as props */}
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor}/>
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor}/>
       </div>
     )
   }
